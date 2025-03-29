d2b59a2008bb47964bb3a75c1e261c2f2b33ce62
kalyankumar999 <avulakalyan2001@gmail.com>
2025-03-29 17:09:44 +0530

    update bio

--- src/components/AboutSection/data.js
+++ src/components/AboutSection/data.js
@@ -4,5 +4,5 @@ export const aboutData = {
   experience: "2 years experience as a Frontend Developer",
   hobbies: "Photography, Coding, and Traveling",
   description:
-    " I am a passionate Software Web Developer with over a year of experience in the industry. Professionally, I'm focused on frontend development, crafting intuitive and dynamic user interfaces. I am dedicated to continuous learning and improvement, always striving to enhance my skills and contribute to impactful projects.",
+    " I am a passionate Software Web Developer with 2 years of experience in the industry. Professionally, I'm focused on frontend development, crafting intuitive and dynamic user interfaces. I am dedicated to continuous learning and improvement, always striving to enhance my skills and contribute to impactful projects.",
 };
